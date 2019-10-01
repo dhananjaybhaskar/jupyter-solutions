671016a1a1e9b4fe4eb748d24822b8cd8df79497
Samuel Watson <samuel.s.watson@gmail.com>
2019-10-01 15:08:25 -0400

    improve code organization

--- jupytersolutions/__init__.py
+++ jupytersolutions/__init__.py
@@ -3,7 +3,45 @@ import json
 import os
 from copy import deepcopy
 
-def write_worksheet(filename=None):
+def clean_cell(cell):
+    if 'execution_count' in cell:
+        cell['execution_count'] = None
+
+def process_cell(cell, keep_keyword, strip_keyword):
+    """
+    Cleans the cell and removes lines which 
+    end with the trigger '# {keyword}'
+    """
+    cell = deepcopy(cell)
+    clean_cell(cell)
+    source = []
+    if len(cell['source']) == 0:
+        return cell
+    if cell['source'][0].startswith(f"# {strip_keyword}"):
+        return None
+    for line in cell['source']:
+        if line.strip().endswith(f"# {strip_keyword}"):
+            continue
+        newline = line.replace(f"# {keep_keyword}","")
+        if line.isspace() or not newline.isspace():
+            source.append(newline)
+    cell['source'] = source
+    return cell
+
+def process_doc(master, 
+                original_doc, 
+                append_string, 
+                keep_keyword, 
+                strip_keyword):
+    cells = [process_cell(cell, keep_keyword, strip_keyword) for cell in original_doc['cells']]
+    cells = [cell for cell in cells if cell is not None]
+    doc = deepcopy(original_doc)
+    doc['cells'] = cells
+    with open(master.replace("-master",append_string),"w") as f:
+        print("Writing " + master.replace("-master",append_string))
+        json.dump(doc,f)
+
+def write_all(filename=None):
     if filename is None:
         for name in os.listdir("."):
             if "-master.ipynb" in name:
@@ -14,42 +52,6 @@ def write_worksheet(filename=None):
         raise Exception("No master file given or found")
     with open(filename) as f:
         doc = json.load(f)
-
-    sol_cells = []
-    worksheet_cells = []
-
-    for cell in doc['cells']:
-        if 'outputs' in cell:
-            cell['outputs'] = []
-        if 'execution_count' in cell:
-            cell['execution_count'] = None
-        worksheet_source = []
-        sol_source = []
-        for line in cell['source']:
-            if line.startswith("# solution"):
-                break
-            if not line.strip().endswith(" # solution"):
-                newline = line.replace("# worksheet","")
-                if line.isspace() or not newline.isspace():
-                    worksheet_source.append(newline)
-        for line in cell['source']:
-            if line.startswith("# worksheet"):
-                break
-            if not line.strip().endswith(" # worksheet"):
-                newline = line.replace("# solution","")
-                if line.isspace() or not newline.isspace():
-                    sol_source.append(newline)
-        cell['source'] = worksheet_source
-        worksheet_cells.append(deepcopy(cell))
-        cell['source'] = sol_source
-        sol_cells.append(deepcopy(cell))
         
-    doc['cells'] = sol_cells
-    with open(filename.replace("-master","-sol"),"w") as f:
-        print("Writing " + filename.replace("-master","-sol"))
-        json.dump(doc, f)
-
-    doc['cells'] = worksheet_cells
-    with open(filename.replace("-master",""), "w") as f:
-        print("Writing " + filename.replace("-master",""))
-        json.dump(doc, f)
+    process_doc(filename, doc, "-sol", "solution", "worksheet") # solutions
+    process_doc(filename, doc, "", "worksheet", "solution") # worksheet
